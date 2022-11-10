0025f18b0a277f2cde5e5529c815622c067001e8
Aleksandar Jauleski <thegeneralmelon12@gmail.com>
2022-11-10 20:53:09 +0100

    Update on login logic.

---
+++ scripts/login_handler.js
@@ -0,0 +1,34 @@
+const login_form = document.getElementById("login-form");
+
+login_form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	let user_name = e.target[0].value;
+	let password = e.target[1].value;
+
+	if (!user_name || !password) {
+		let msg = "Please enter both fields";
+		console.log(msg);
+
+		return;
+	}
+
+	fetch("http://localhost:3000/users/login", {
+		method: "POST",
+
+		body: JSON.stringify({
+			user_name,
+			password,
+		}),
+
+		headers: {
+			"Content-type": "application/json; charset=UTF-8",
+		},
+
+		// credentials: "include",
+	})
+		.then((res) => res.json())
+		.then((data) => {
+			console.log(data);
+		});
+});
